3a190ddf0b2871918ae2ec1fa262df80dde058f4
Darshan <darshancsagar21@gmail.com>
2025-08-29 19:10:19 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,201 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Software Developer",
+    icon: web,
+  },
+  {
+    title: "Software Engineer",
+    icon: mobile,
+  },
+
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+
+];
+
+const experiences = [
+  {
+    title: "Software Developer",
+    company_name: "Zibtek",
+   // icon: starbucks,
+    iconBg: "#383E56",
+    date: "December 2022 - January 2024",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Improving the performance of application by reducing CLS and LCP."
+    ],
+  },
+   {
+    title: "Software Engineer",
+    company_name: "Trinetium Tech Pvt Ltd",
+    //icon: starbucks,
+    iconBg: "#383E56",
+    date: "December 2022 - January 2024",
+    points: [
+      "Worked on Material UI library.", 
+"Worked on adding app tour(what’s new) feature in the product.", 
+"Worked on integrating google places api for places nearby search.", 
+"Worked on integrating trip advisor location reviews apis using api routes in Next.js.", 
+"Worked developing car rental module from scratch.",
+"Worked on creating typeable date in date format.",
+"Worked on creating search optimization by implementing prefetch which has increased the smooth user experience." 
+    ],
+  },
+
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Zibtek nextJS website",
+    description: "This is website of the Zibtek company which we migrated from angular to Next.js.Here I created each pages from scratch with file based routing. Also integrated forms using React-hook-form and zod schema validation. Also Created Re-useable components.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Nextjs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    //image: carrent,
+   // source_code_link: "https://github.com/",
+  },
+
+  {
+    name: "Golden Ring travel",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "material-ui",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    //image: tripguide,
+    //source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
